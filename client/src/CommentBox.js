f2655cfc5f474d910849a6f84e02f2b64e29cb3c
Shaun Savage <swansavage@gmail.com>
2018-05-04 19:54:18 -0400

    comment box component showing comments from db

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from './data';
+
 import './CommentBox.css';
 
 class CommentBox extends Component {
     constructor() {
         super();
-        this.state = { data: [] };
+        this.state = {
+            data: [],
+            error: null,
+            author: '',
+            text: ''
+         };
+
+         this.pollInterval = null;
+    }
+
+
+    componentDidMount(){
+        this.loadCommentsFromServer();
+}
+
+    loadCommentsFromServer(){
+        fetch('/api/comments/').then(
+            data => data.json()
+        ).then(
+            (res) => {
+                if (!res.success) this.setState({error: res.error});
+                else this.setState({data: res.data});
+            }
+        );
     }
     render() {
         return (
             <div className="container">
             <div className="comments">
             <h2>Comments:</h2>
-            <CommentList data={DATA} />
+            <CommentList data={this.state.data} />
             </div>
             <div className="form">
-            <CommentForm />
+            <CommentForm author={this.state.author} text={this.state.text} />
             </div>
+
+            {this.state.error && <p>{this.state.error}</p>}
+
             </div>
         );
     }
