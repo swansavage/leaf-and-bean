643654a82620feea79bf8d66571992a368e7e350
Shaun Savage <swansavage@gmail.com>
2018-05-08 21:30:19 -0400

    final heroku test build, for real this time

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   state = {users: []}
 
   componentDidMount() {
-    fetch('api/users')
+    fetch('/api/users')
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
