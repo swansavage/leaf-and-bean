899753cdf43f31c7b9d7e49de1e637401138faf3
Shaun Savage <swansavage@gmail.com>
2018-05-08 23:08:05 -0400

    added seed to component

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,16 @@ class App extends Component {
   state = {products: []}
 
   componentDidMount() {
+
+     fetch('/api/products/seed')
+        .then(res => res.json())
+
+
     fetch('/api/products')
       .then(res => res.json())
       .then(products => this.setState({ products }));
+
+
   }
 
   render() {
